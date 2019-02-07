9cfe9201b4bd7506a7a2767c05d29338a39f87c1
Kunal Agarwal <kunal.ajay@gmail.com>
2019-02-07 00:05:40 -0800

    Initial Commit

---
+++ TipTopTests/TipTopTests.swift
@@ -0,0 +1,34 @@
+//
+//  TipTopTests.swift
+//  TipTopTests
+//
+//  Created by Kunal Agarwal on 07/02/19.
+//  Copyright © 2019 Kunal. All rights reserved.
+//
+
+import XCTest
+@testable import TipTop
+
+class TipTopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
